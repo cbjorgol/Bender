f075460a154e8c75b730bee91a60e2998f279ef9
CHRISTOPHER BJORGOLFSSON <christopherbjorgolfsson@CHRISTOPHERs-MacBook-Air-2.local>
2016-07-20 22:31:59 -0400

    updating mail script for looping

--- mail.py
+++ mail.py
@@ -6,7 +6,7 @@ from chuck_norris_jokes import say_chuck_norris_joke
 from cat_facts import say_cat_fact
 from robot_keys import account, password
 from weather import get_weather
-from test_body import run_forward
+# from test_body import move
 
 class mailmanager():
     def __init__(self, account, password):
@@ -40,45 +40,65 @@ class mailmanager():
             return original
 
 def get_payload(original):
+    """
+    This needs to be able to handle payloads
+      when multiple emails come through
+
+    Parameters
+    ----------
+    original
+
+    Returns
+    -------
+
+    """
+    # TODO: Set up multiple email handling
     if len(original) > 1:
-        print "> 1"
-        print original.get_payload()
         payload = original.get_payload()
     else:
-        print "one len"
         payload = original.get_payload()
     print payload
     return payload
+
+
+def check_mail(mail):
+    mail_contents = mail.check_mail()
+    if mail_contents is not None:
+        if mail_contents['Subject'] is None:
+            speak(mail_contents.get_payload())
+
+        elif 'Subject' in mail_contents:
+
+            chuck_norris_request = mail_contents['Subject'].lower().find('chuck norris') != -1
+            cat_fact_request = mail_contents['Subject'].lower().find('cat fact') != -1
+            weather_request = mail_contents['Subject'].lower().find('weather') != -1
+
+            if chuck_norris_request:
+                say_chuck_norris_joke()
+            elif cat_fact_request:
+                say_cat_fact()
+            elif weather_request:
+                get_weather()
+            else:
+                speak(get_payload(mail_contents))
+    else:
+        print "no facts found"
+
 
 if __name__ == '__main__':
 
 
     mail = mailmanager(account, password)
+    mail_wait = 20
 
-    for i in range(50):
-        mail_contents = mail.check_mail()
-        if mail_contents is not None:
-            if mail_contents['Subject'] is None:
-                speak(mail_contents.get_payload())
-
-            elif 'Subject' in mail_contents:
-                
-                chuck_norris_request = mail_contents['Subject'].lower().find('chuck norris') != -1
-                cat_fact_request = mail_contents['Subject'].lower().find('cat fact') != -1
-                weather_request = mail_contents['Subject'].lower().find('weather') != -1
-
-                if chuck_norris_request:
-                    run_forward(100, 5)
-                    say_chuck_norris_joke()
-                    run_forward(-100, 7)
-                elif cat_fact_request:
-                    say_cat_fact()
-                elif weather_request:
-                    run_forward(-100, 7)
-                    get_weather()
-                    run_forward(100, 7)
-                else:
-                    speak(get_payload(mail_contents))
-        else:
-            print "no facts found"
-        time.sleep(20)
+    t0 = time.time()
+    time_since_mail = 10
+    while True:
+        time_since_mail = time.time() - t0
+        if time_since_mail > mail_wait:
+            check_mail(mail)
+            time_since_mail = mail_wait
+            t0 = time.time()
+        print "movement stuff"
+        # move(60, 20, seconds=0)
+        time.sleep(1)
--- test_body.py
+++ test_body.py
@@ -1,33 +1,35 @@
 
 
 from BrickPi import *
+import time
 
 BrickPiSetup()  										# setup the serial port for communication
 ############################################
 # !  Set the sensor type on the line below.  
 BrickPi.SensorType[PORT_1] = TYPE_SENSOR_TOUCH   	#Set the type of sensor at PORT_1.  NXT Touch Sensor.
 BrickPi.SensorType[PORT_2] = TYPE_SENSOR_EV3_TOUCH_0  	#Set the type of sensor at PORT_2.  EV3 Touch sensor.
-BrickPi.MotorEnable[PORT_A] = 1 #Enable the Motor A
+
+
+
 BrickPi.MotorEnable[PORT_B] = 1 #Enable the Motor B
 BrickPi.MotorEnable[PORT_C] = 1 #Enable the Motor A
-BrickPi.MotorEnable[PORT_D] = 1 #Enable the Motor B
 
 
 
-def run_forward(speed=200, seconds=3):
-    print "Running Forward"
-    BrickPi.MotorSpeed[PORT_A] = speed  #Set the speed of MotorA (-255 to 255)
+def move(speed=200, right_speed=None, seconds=3):
+
+    if right_speed==None:
+        right_speed = speed
+
     BrickPi.MotorSpeed[PORT_B] = speed  #Set the speed of MotorB (-255 to 255)
-    BrickPi.MotorSpeed[PORT_C] = speed  #Set the speed of MotorA (-255 to 255)
-    BrickPi.MotorSpeed[PORT_D] = speed  #Set the speed of MotorB (-255 to 255)
+    BrickPi.MotorSpeed[PORT_C] = right_speed  #Set the speed of MotorA (-255 to 255)
 
     ot = time.time()
     while(time.time() - ot < seconds):    #running while loop for 3 seconds
         BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
-	time.sleep(.001)
+    time.sleep(.001)
+
+
 
 if __name__ == '__main__':
-    run_forward(60, 0.5)
-    run_forward(-60, 0.5)
-    run_forward(60, 0.5)
-    run_forward(-60, 0.5)
+    move(60, 20,  seconds=0.8)
