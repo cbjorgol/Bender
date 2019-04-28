26bcb63ba51dfae0eaf48df87e7488ce390272bc
unknown <cbjorgol@gmail.com>
2019-04-28 11:20:33 -0400

    changing refresh time

--- face_detection2.py
+++ face_detection2.py
@@ -19,27 +19,27 @@ face_cascade = cv2.CascadeClassifier('/home/pi/opencv/data/haarcascades/haarcasc
 
 t0 = time.time()
 time_since_update = 9999
-while(cap.isOpened()):
+while (cap.isOpened()):
     ret, image = cap.read()
 
-    if ret==True:
-        re_colored=cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        faces = face_cascade.detectMultiScale(re_colored, 1.22, 2, flags = cv2.CASCADE_SCALE_IMAGE)
+    if ret == True:
+        re_colored = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        faces = face_cascade.detectMultiScale(re_colored, 1.3, 2, flags=cv2.CASCADE_SCALE_IMAGE)
         print("Humans Actively Monitored:", len(faces))
-        for (x,y,w,h) in faces:
-            cv2.rectangle(re_colored,(x,y),(x+w,y+h),(255,255,0), 2)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(re_colored, (x, y), (x + w, y + h), (255, 255, 0), 2)
 
-        
+
         def command():
             return subprocess.Popen(['python', 'chuck_norris_jokes.py'])
 
+
         if len(faces) > 1:
             time_since_update = time.time() - t0
-            if time_since_update > 60:
+            if time_since_update > 20:
                 t0 = time.time()
                 command()
-            
-                        
+
         # Display the resulting frame
         cv2.imshow('frame', re_colored)
         if cv2.waitKey(1) & 0xFF == ord('q'):
