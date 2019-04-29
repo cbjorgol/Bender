7669f825220fffaa5e1a00affd860d56589a7cf8
unknown <cbjorgol@gmail.com>
2019-04-28 20:42:23 -0400

    new css

--- flask_test.py
+++ flask_test.py
@@ -10,17 +10,18 @@ app = Flask(__name__)
 
 NPV_DIFF = 80000000
 FREQ = "Today's"
-REF_DATE = datetime(2017, 9, 30, 0, 0)
+REF_DATE = datetime(2019, 1, 1, 0, 0)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def get_value(npv_diff):
+def get_value(npv_diff, ref_date=None):
     npv_per_period = npv_diff / 365.
 
     # This is the index date/time
-    ref_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    if ref_date is None:
+        ref_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     multiplier = 1
     time_this_week = datetime.now() - ref_date
     pct_of_day = time_this_week.total_seconds() / float(multiplier * 24 * 60 * 60)
@@ -29,19 +30,19 @@ def get_value(npv_diff):
 @app.route('/time_feed')
 def time_feed():
     def generate():
-        yield get_value(NPV_DIFF)  # return also will work
+        yield get_value(NPV_DIFF, ref_date=REF_DATE)  # return also will work
     return Response(generate(), mimetype='text')
 
 @app.route('/time_feed2')
 def time_feed2():
     def generate():
-        yield get_value(NPV_DIFF*1.2)  # return also will work
+        yield get_value(NPV_DIFF*1.2, ref_date=REF_DATE)  # return also will work
     return Response(generate(), mimetype='text')
 
 @app.route('/time_feed3')
 def time_feed3():
     def generate():
-        yield get_value(NPV_DIFF*0.3) # return also will work
+        yield get_value(NPV_DIFF*0.2, ref_date=REF_DATE) # return also will work
     return Response(generate(), mimetype='text')
 
 if __name__ == '__main__':
